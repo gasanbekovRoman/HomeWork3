feab2292e34f1de95ec83167c2ef7f70de7fc335
GasanbekovRoman <spamerl256@gmail.com>
2021-10-31 18:29:02 +0400

    sd

---
+++ MVC-N/MVC-N/Comments/Controller/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  MVC-N
+//
+//  Created by Asya  on 31.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var tableView : UITableView!
+
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
